18ef738133d3f9f2d04924591cd27c93c9ecf90a
Zac Heisey <zheisey@gmail.com>
2018-10-10 11:26:00 -0400

    complete script

--- main.js
+++ main.js
@@ -14,8 +14,11 @@ var monsters = [
 	'monster11.svg'
 ];
 
-// Get user's monster count
+// Set user's monster count
 var monsterCount = 0;
+
+// Get user's collected monster count span field
+var collectedMonsters = document.querySelector('#collected-monsters');
 
 // Get the app div
 var appContent = document.getElementById('app');
@@ -65,9 +68,40 @@ var loadGame = function() {
 
 // Playing the game
 var playGame = function(event) {
+	// If user clicks on a door, reveal a monster or socks
 	if (event.target.classList.contains('door-img')) {
 		event.target.setAttribute('hidden', true);
 		event.target.nextSibling.removeAttribute('hidden');
+	}
+	// If monster image is shown, add to monsterCount
+	if (event.target.nextSibling.src.includes('/images/monster')) {
+		monsterCount++;
+		collectedMonsters.textContent = monsterCount;
+		// Show WIN message if user collects 11 monsters
+		if (monsterCount === 11) {
+			setTimeout(function() {
+				var playAgain = confirm('WINNER! You are a Master Monster Wrangler! Want to play again?');
+				// Reload page if user wants to play again
+				if (playAgain === true) {
+					window.location.reload(true);
+				} else {
+					// Show Bye Boo GIF
+					appContent.innerHTML = '<img src="images/bye-boo.gif">';
+				}
+			}, 500);
+		}
+	} else {
+		// Show LOSE message if user select the socks
+		setTimeout(function() {
+			var playAgain = confirm('MUAHAHAHA! You\'ve been socked! Want to try again?');
+			if (playAgain === true) {
+				// Reload page if user wants to play again
+				window.location.reload(true);
+			} else {
+				// Show Bye Boo GIF
+				appContent.innerHTML = '<img src="images/bye-boo.gif">';
+			}
+		}, 500);
 	}
 }
 
